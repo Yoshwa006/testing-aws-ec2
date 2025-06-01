355fcc4e223b970eb63ffdda2c997ac0fe435a39
yoshwa <ryoshwaa@gmail.com>
2025-06-01 23:42:17 +0530

    Added local files

---
+++ src/main/java/com/example/testing_aws/TestingAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingAwsApplication.class, args);
+	}
+
+}
